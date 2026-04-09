bbac20f57c5b74c0524d3e5df5111ad13752a0d2
dfnc-test <redsnowcp@gmail.com>
2026-04-09 19:25:54 -0400

    Create helpers.py

---
+++ flippers/utils/helpers.py
@@ -0,0 +1,3 @@
+def get_image_url(name):
+    formatted = name.replace(" ", "_").replace("'", "").replace("(", "").replace(")", "")
+    return f"https://oldschool.runescape.wiki/images/{formatted}.png"
